f381065f335efffd47a0fb423dd30d7cb4276f94
Jose Ramos <jramoscolon@yahoo.com>
2017-02-15 11:03:16 -0400

    added comments in registros controller

--- app/controllers/registros_controller.rb
+++ app/controllers/registros_controller.rb
@@ -4,7 +4,7 @@ class RegistrosController < ApplicationController
   # GET /registros
   # GET /registros.json
   def index
-    @registros = Registro.paginate(page: params[:page], per_page: 5)
+    @registros = Registro.paginate(page: params[:page], per_page: 5) # unless request.format == 'xls'
     respond_to do |format|
       format.html
       format.csv { send_data text: @registros.to_csv }
