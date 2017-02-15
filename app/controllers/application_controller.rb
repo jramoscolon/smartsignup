305dcec8c682275e29cfb507328e41056dbbaf8e
Jose Ramos <jramoscolon@yahoo.com>
2017-02-15 13:43:24 -0400

    styled homepage, and redirected loggedin users to home

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,9 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+
+  def logged_in?
+    # "!!" converts the "current_user" variable to a boolean (exists or doesn't)
+    !!current_user
+  end
 end
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,6 @@
 class StaticPagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index]
   def index
+  	redirect_to registros_path if logged_in?
   end
 end
